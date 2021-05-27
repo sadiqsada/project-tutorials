72c22a5ccdb14e19805874235445ecf00ba3e74f
Sadiq <samin.sada@gmail.com>
2021-05-27 14:01:57 -0400

    add Tasks component

--- vue/task-tracker/src/App.vue
+++ vue/task-tracker/src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div class="container">
-      <Header title="Task Tracker"/>
+    <Header title="Task Tracker" />
+    <Tasks :tasks="tasks" />
   </div>
 </template>
 
 <script>
-import Header from './components/Header';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Header
+    Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctors Appointment",
+        day: "March 5th at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Meeting with boss",
+        day: "March 6th at 1:30pm",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Food shopping",
+        day: "March 7th at 2:00pm",
+        reminder: false,
+      },
+    ];
   },
 };
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
 * {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
 body {
-  font-family: 'Poppins', sans-serif;
+  font-family: "Poppins", sans-serif;
 }
 .container {
   max-width: 500px;
--- vue/task-tracker/src/components/Button.vue
+++ vue/task-tracker/src/components/Button.vue
@@ -1,13 +1,20 @@
 <template>
-    <button :style="{background: color}" class="btn">{{text}}</button>
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
 </template>
 
 <script>
 export default {
-    name: 'Button',
-    props: {
-        text: String,
-        color: String
-    }
-}
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      console.log("click");
+    },
+  },
+};
 </script>
